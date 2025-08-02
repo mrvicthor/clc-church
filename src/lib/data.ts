a8779d86927ee57f474e8dc2fc5ae64196aef92a
Victor Eleanya <victoreleanya89@gmail.com>
2025-08-02 06:41:48 +0100

    feat(data.ts): update data

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -4,7 +4,7 @@ export const events = [
   {
     id: 1,
     title: "Sunday Worship Service",
-    date: "2025-07-20",
+    date: "2025-08-03",
     time: "10:00 AM",
     location: "Haris Academy, South Norwood Cumber Avenue. SE25 6AE",
     description:
@@ -15,7 +15,7 @@ export const events = [
   {
     id: 2,
     title: "Wednesday Bible Study",
-    date: "2025-07-23",
+    date: "2025-08-06",
     time: "7:30 PM",
     location: "Online - YouTube Live",
     description:
@@ -26,7 +26,7 @@ export const events = [
   {
     id: 3,
     title: "Relate Group",
-    date: "2024-01-27",
+    date: "2025-08-09",
     time: "9:00 AM",
     location: "Online - Zoom",
     description:
@@ -37,7 +37,7 @@ export const events = [
   {
     id: 4,
     title: "Prayer Meeting",
-    date: "2024-01-31",
+    date: "2025-08-12",
     time: "6:30 PM",
     location: "Online - Zoom",
     description:
