d95e10ffd6ab0d87a008bf86b59bde406431c0cd
Victor Eleanya <victoreleanya89@gmail.com>
2026-05-25 15:53:35 +0100

    update(data.ts):  update event date

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -7,7 +7,7 @@ export const events = [
   {
     id: 1,
     title: "Sunday Worship Service",
-    date: "2026-05-24",
+    date: "2026-05-31",
     time: "10:00 AM",
     location: "Haris Academy, South Norwood Cumber Avenue. SE25 6AE",
     description:
